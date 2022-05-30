374c3a47216feec024d2f299f5b4fe28ad312ce4
Bor Praznik <praznik.bor@gmail.com>
2022-05-30 17:45:42 +0200

    add database

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
-import datetime
+import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from sqla_wrapper import SQLAlchemy
 
 app = Flask(__name__)
 
+db_url = os.getenv("DATABASE_URL", "sqlite:///db.sqlite").replace("postgres://", "postgresql://", 1)
+db = SQLAlchemy(db_url)
 
-@app.route("/")
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String, unique=False)
+    text = db.Column(db.String, unique=False)
+
+
+db.create_all()
+
+
+@app.route("/", methods=["GET"])
 def index():
-    some_text = "Message from the handler!"
-    current_year = datetime.datetime.now()
-    return render_template("index.html", text=some_text, current=current_year)
+    messages = db.query(Message).all()
+    return render_template("index.html", messages=messages)
 
 
-@app.route("/about-me")
-def about_me():
-    about_list_1 = []
-    return render_template("about.html", about_list=about_list_1)
+@app.route("/add-message", methods=["POST"])
+def add_message():
+    username = request.form.get("username")
+    message = request.form.get("text")
+
+    message = Message(author=username, text=message)
+    message.save()
+    return redirect("/")
 
 
 if __name__ == '__main__':
-    app.run(use_reloader=True)
+    app.run()
